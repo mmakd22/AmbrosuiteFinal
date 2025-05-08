d7c1553b795d408ba0f85c5ee5caed74d4b1c0e6
Matías Ezequiel Molina <matiasemolina1@gmail.com>
2025-05-08 18:46:58 -0300

    readme
    Se agrega el readme con instrucciones para el deploy

---
+++ AmbrosuiteApp/readme.md
@@ -0,0 +1,17 @@
+
+Como iniciar la app
+
+Versiones: NodeJs 20.18.0
+
+
+
+**Versión Web**
+
+Prender proyecto .NET
+
+Abrir consola en la carpeta AmbrosuiteAPP y tirar estos comandos
+
+npm i
+
+npx expo start
+
--- AmbrosuiteApp/src/utils/config.ts
+++ AmbrosuiteApp/src/utils/config.ts
@@ -1,2 +1,2 @@
 //Cambiar url api cada vez que se genere con ngrok
-export const API_BASE_URL = 'https://localhost:5001';
+export const API_BASE_URL = 'https://afd7-2803-9800-9005-8210-a548-f3fa-c19c-258.ngrok-free.app';
