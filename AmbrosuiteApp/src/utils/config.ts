1159d99df70f13868e4f55556c5850d8379961d8
JooGo01 <joseakd136@gmail.com>
2025-07-01 16:48:11 -0300

    cambios url api, ngrok

--- AmbrosuiteApp/src/utils/config.ts
+++ AmbrosuiteApp/src/utils/config.ts
@@ -1,2 +1,2 @@
 //Cambiar url api cada vez que se genere con ngrok
-export const API_BASE_URL = 'https://9b6c-2800-810-49e-1de7-7dff-eaf7-e79e-933.ngrok-free.app';
+export const API_BASE_URL = 'https://31eb-2a02-4780-14-1af8-00-1.ngrok-free.app';
