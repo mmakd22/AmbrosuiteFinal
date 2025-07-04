7a8b2ecfdcf03ee320726407afb8f077ceb6d4fb
JooGo01 <joseakd136@gmail.com>
2025-07-03 21:52:45 -0300

    cambios en el apptsx para que muestre mas mensajes y ver donde corta

--- AmbrosuiteApp/App.tsx
+++ AmbrosuiteApp/App.tsx
@@ -2,24 +2,47 @@ import React, { useEffect, useState } from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import AppNavigator from './src/navigation/AppNavigator';
 import { getToken, getRole } from './src/utils/auth';
+import { LogBox } from 'react-native';
+import { View, Text } from 'react-native';
+
+
+LogBox.ignoreLogs(["Tried to enqueue runnable"]); // evitar spam innecesario
+
+// Captura errores JS no atrapados
+ErrorUtils.setGlobalHandler((error, isFatal) => {
+  console.log("ERROR GLOBAL:", error.message, error.stack);
+});
 
 export default function App() {
   const [isAuthenticated, setIsAuthenticated] = useState<boolean | null>(null);
 
   useEffect(() => {
     const checkAuth = async () => {
-      const token = await getToken();
-      const rol = await getRole();
-      if (token && (rol === 0 || rol === 1)) {
-        setIsAuthenticated(true);
-      } else {
+      try {
+        console.log("Verificando autenticación...");
+        const token = await getToken();
+        const rol = await getRole();
+        if (token && (rol === 0 || rol === 1)) {
+          setIsAuthenticated(true);
+        } else {
+          setIsAuthenticated(false);
+        }
+      }catch (error) {
+        console.error("Error al verificar autenticación:", error);
         setIsAuthenticated(false);
       }
     };
     checkAuth();
   }, []);
-
-  if (isAuthenticated === null) return null; // Evita renderizar antes de saber el auth
+  
+  // Evita renderizar antes de saber el auth
+  if (isAuthenticated === null) {
+    return (
+      <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Text>Cargando...</Text>
+      </View>
+    );
+  }
 
   return (
     <NavigationContainer>
